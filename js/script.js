b5eeb7a0e4edd22c48d8af8f4dd685fe8c7f863e
Fady-Fouad <111523809+Fady8080@users.noreply.github.com>
2022-10-19 04:32:43 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// const modal = document.querySelector(".custom-modal-container");
+// const input = document.querySelector("#myInput");
+// const button = document.querySelector("#sendButton");
+// const closeBtn = document.querySelector(".close-btn");
+// const spanContent = document.querySelector(".span-content");
+
+// function buttonFunction() {
+//   console.log(input.value);
+//   modal.style.display = "flex";
+//   spanContent.innerHTML = input.value;
+// }
+
+// button.addEventListener("click", buttonFunction);
+
+// closeBtn.addEventListener('click', () => {
+//   modal.style.display = 'none';
+// })
